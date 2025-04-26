422d66bff34f923d9e5a2f07fd866fe4c2cb6af7
sandyflat <chapsandip17@gmail.com>
2025-04-26 16:13:12 +0545

    basic change in title

--- JS/main.js
+++ JS/main.js
@@ -51,7 +51,7 @@ ScrollReveal().reveal('.home-contact p, .about-content', {origin: 'right'});
 
 // typed js
 const typed = new Typed('.multiple-text', {
-    strings: ['Backend Developer', 'Student',],
+    strings: ['Backend Developer', 'Problem Solver',],
     typeSpeed: 70,
     backSpeed: 70,
     backDelay: 1000,
